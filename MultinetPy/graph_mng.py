41d421afe2e9119b55fa5fe16b57fc71e82e1b9b
alirezaf2001 <68161296+alirezaf2001@users.noreply.github.com>
2025-02-12 08:45:04 -0500

    Update graph_mng.py
    update bugs for not being int

--- MultinetPy/graph_mng.py
+++ MultinetPy/graph_mng.py
@@ -7,13 +7,13 @@ class Import_Graph:
         pass
 
     @staticmethod
-    def make_graph(node_file, edges_file, layers_file=None):
+    def make_graph(node_file, edges_file, layers_file=None, inter_layers_file=None):
         nodes = []
         pos = {}
         file = open(node_file)
         for line in file:
             node = line.split(" ")
-            nodes.append((node[0], {"name": node[1]}))
+            nodes.append((int(node[0]), {"name": node[1]}))
         file.close()
 
         layers = []
@@ -21,13 +21,13 @@ class Import_Graph:
             file = open(layers_file)
             for line in file:
                 layer = line.split(" ")
-                layers.append((layer[0], layer[1]))
+                layers.append((int(layer[0]), layer[1]))
 
         edges = []
         file = open(edges_file)
         for line in file:
             edge = line.split(" ")
-            edges.append((edge[0], (edge[1], edge[2], {"weight": int(edge[3][:-1])})))# after removing any trailing characters from the original string.
+            edges.append((int(edge[0]), (int(edge[1]), int(edge[2]), {"weight": int(edge[3][:-1])})))# after removing any trailing characters from the original string.
 # If layers is not provided (though the condition len(layers) < 0 will always be false), creates a unique set of layer IDs from the edges and converts it to a list
 
         if len(layers) < 0:
@@ -41,7 +41,7 @@ class Import_Graph:
             temp_graph.add_edges_from(edge[1] for edge in edges if edge[0] == layer[0])
             graphs.append(temp_graph)
 
-        mg = mp.MultiNetPy(list_of_layers=graphs)
+        mg = mp.MultiNetPy(list_of_layers=graphs, inter_adjacency_matrix=inter_layers_file)
         return mg
 
 
